5d2ff17f4fa966cd1eba091326a135b5401ee8bd
oldboni22 <oldboni22@gmail.com>
2026-01-24 19:38:32 +0300

    Implement pagination

---
+++ server/PryaniRchive/Common/Pagination/PagedList.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Common.Pagination;
+
+public record PagedList<T>(List<T> Items, int PageNumber, int PageSize, int PageCount, int TotalCount)
+{
+    public bool HasPreviousPage => PageNumber > 1;
+    
+    public bool HasNextPage => PageNumber < TotalCount;
+}
