90c3b73df4c2707116ec454f9733a146ea86aff6
oldboni22 <oldboni22@gmail.com>
2026-01-25 11:22:58 +0300

    Add DbSets

--- server/PryaniRchive/AuthService.Infrastructure/Data/EntityConfiguration/AuthServiceDbContext.cs
+++ server/PryaniRchive/AuthService.Infrastructure/Data/EntityConfiguration/AuthServiceDbContext.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthService.Infrastructure.Data.EntityConfiguration;
 
 public class AuthServiceDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<AuthenticationCredentials>  AuthenticationCredentials { get; set; }
+    
+    public DbSet<UserSession> UserSessions { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AuthServiceDbContext).Assembly);
     }
 }
-
