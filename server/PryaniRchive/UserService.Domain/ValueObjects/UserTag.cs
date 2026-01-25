26e91011d103aba623aea8675f21ffbb91428f18
oldboni22 <oldboni22@gmail.com>
2026-01-25 12:50:32 +0300

    update user tag value object

--- server/PryaniRchive/UserService.Domain/ValueObjects/UserTag.cs
+++ server/PryaniRchive/UserService.Domain/ValueObjects/UserTag.cs
@@ -8,18 +8,15 @@ public record UserTag
     
     public string Value { get; init; }
     
-    private UserTag() {}
+    private UserTag(string value) => Value = value;
 
     public static Result<UserTag> Create()
     {
         var bytes = RandomNumberGenerator.GetBytes(Size / 2);
-        return new UserTag
-        {
-            Value = Convert.ToHexString(bytes).ToLower()
-        };
+        return new UserTag(Convert.ToHexString(bytes).ToLower());
     }
 
-    public static UserTag FromDatabase(string value) => new UserTag {Value = value};
+    public static UserTag FromDatabase(string value) => new UserTag(value);
     
     public override string ToString() => Value;
     
