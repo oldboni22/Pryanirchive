08e41ab0ca5e64bce817dfef61a6bcd5c5105d22
oldboni22 <oldboni22@gmail.com>
2026-02-05 13:31:03 +0300

    Implemented cached resource

---
+++ server/PryaniRchive/Common/Cache/ICachedResource.cs
@@ -0,0 +1,53 @@
+using Microsoft.Extensions.Caching.Hybrid;
+
+namespace Common.Cache;
+
+public interface ICachedResource<in TKey, TValue>
+{
+    Task<TValue> GetAsync(TKey key, CancellationToken cancellationToken = default);
+
+    Task SetAsync(TKey key, TValue value, CancellationToken cancellationToken = default);
+    
+    Task CreateAsync(TKey key, TValue value, CancellationToken cancellationToken = default);
+    
+    Task RemoveAsync(TKey key, CancellationToken cancellationToken = default);
+}
+
+public abstract class CachedResource<TKey, TValue>(HybridCache cache) : ICachedResource<TKey, TValue>
+{
+    protected HybridCache Cache { get; } = cache;
+    
+    protected abstract ValueTask<TValue> GetFromResourceAsync(TKey key, CancellationToken cancellationToken = default);
+    
+    protected abstract ValueTask DeleteFromResourceAsync(TKey key, CancellationToken cancellationToken = default);
+    
+    protected abstract ValueTask SetResourceAsync(TKey key, TValue value, CancellationToken cancellationToken = default);
+    
+    protected abstract ValueTask CreateResourceAsync(TKey key, TValue value, CancellationToken cancellationToken = default);
+    
+    public async Task<TValue> GetAsync(TKey key, CancellationToken cancellationToken = default)
+    {
+        return await Cache.GetOrCreateAsync(
+            key.ToString(),
+            ctx => GetFromResourceAsync(key, ctx),
+            cancellationToken: cancellationToken);
+    }
+
+    public async Task SetAsync(TKey key, TValue value, CancellationToken cancellationToken = default)
+    {
+        await SetResourceAsync(key, value, cancellationToken);
+        await Cache.SetAsync(key.ToString(), value, cancellationToken: cancellationToken);
+    }
+
+    public async Task CreateAsync(TKey key, TValue value, CancellationToken cancellationToken = default)
+    {
+        await  CreateResourceAsync(key, value, cancellationToken);
+        await Cache.SetAsync(key.ToString(), value, cancellationToken: cancellationToken);
+    }
+
+    public async Task RemoveAsync(TKey key, CancellationToken cancellationToken = default)
+    {
+        await DeleteFromResourceAsync(key, cancellationToken);
+        await Cache.RemoveAsync(key.ToString(), cancellationToken);
+    }
+}
