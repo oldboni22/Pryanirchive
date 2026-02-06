98782435130ab3402561388b369635bf7054da87
oldboni22 <oldboni22@gmail.com>
2026-02-06 21:15:04 +0300

    Fixed avatar service

--- server/PryaniRchive/FileService.Application/Contracts/Blob/ICachedAvatarService.cs
+++ server/PryaniRchive/FileService.Application/Contracts/Blob/ICachedAvatarService.cs
@@ -4,9 +4,9 @@ namespace FileService.Application.Contracts.Blob;
 
 public interface ICachedAvatarService
 {
-    Task<Result<string>> GetAsync(string key, CancellationToken cancellationToken = default);
+    Task<Result<string>> GetAsync(Guid userId, string key, CancellationToken cancellationToken = default);
     
-    Task<Result> SetAsync(string key, Stream value, string contentType, CancellationToken cancellationToken = default);
+    Task<Result<string>> SetAsync(Guid userId, string key, Stream value, string contentType, CancellationToken cancellationToken = default);
     
-    Task<Result> RemoveAsync(string key, CancellationToken cancellationToken = default);
+    Task<Result> RemoveAsync(Guid userId, string key, CancellationToken cancellationToken = default);
 }
