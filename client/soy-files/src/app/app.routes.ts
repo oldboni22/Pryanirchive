b97ddccc159d8efe4ed66fe86497e4ef1f230eca
oldboni22 <oldboni22@gmail.com>
2026-02-09 15:31:27 +0300

    Added router. Added login screen base

--- client/soy-files/src/app/app.routes.ts
+++ client/soy-files/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginScreen} from './features/login-screen/login-screen';
+import {Desktop} from './features/desktop/desktop';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginScreen},
+  { path: 'desktop', component: Desktop},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
--- client/soy-files/src/app/components/box/box.ts
+++ client/soy-files/src/app/components/box/box.ts
@@ -14,7 +14,13 @@ export class Box {
   class = input<string>('', { alias: 'class' });
   fill = input<boolean>(false);
   bordered = input<boolean>(false);
+
   transparent = input<boolean>(false);
+
+  background = input<string | null>(null);
+
+  private bgcolor = computed(
+    () => this.background() ? this.background() : 'bg-bg-color');
 
   combinedClasses = computed(() => {
     return [
@@ -23,7 +29,7 @@ export class Box {
         ? `border-border-color border-[6px] rounded-lg bg-none
          shadow-volume transition-all duration-300 overflow-hidden`
         : '',
-      this.transparent() ? 'bg-transparent' : 'bg-bg-color',
+      this.transparent() ? 'bg-transparent' : this.bgcolor(),
       this.class()
     ].filter(Boolean).join(' ');
   });
